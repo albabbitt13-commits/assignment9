59cf36bf0922bae04b84e2d1faa58cb6704978ff
bruhzbtc <zwbabbitt@gmail.com>
2026-02-13 20:09:04 -0500

    Completed social network graph project

--- social_network.py
+++ social_network.py
@@ -1,29 +1,70 @@
 class Person:
-    '''
-    A class representing a person in a social network.
-    Attributes:
-        name (str): The name of the person.
-        friends (list): A list of friends (Person objects).
-    Methods:
-        add_friend(friend): Adds a friend to the person's friend list.
-   '''
-    
-    pass # Delete this line to implement the Person class
+    def __init__(self, name):
+        self.name = name
+        self.friends = []
+
+    def add_friend(self, friend):
+        # Prevent duplicate friendships
+        if friend not in self.friends:
+            self.friends.append(friend)
 
 
 class SocialNetwork:
-    '''
-    A class representing a social network.
-    Attributes:
-        people (dict): A dictionary mapping names to Person objects.
-    Methods:
-        add_person(name): Adds a new person to the network.
-        add_friendship(person1_name, person2_name): Creates a friendship between two people.
-        print_network(): Prints the names of all people and their friends.
-    '''
-    
-    pass # Delete this line to implement the SocialNetwork class
+    def __init__(self):
+        # Adjacency list representation
+        self.people = {}
+
+    def add_person(self, name):
+        if name in self.people:
+            print(f"{name} already exists in the network.")
+        else:
+            self.people[name] = Person(name)
+
+    def add_friendship(self, person1_name, person2_name):
+        if person1_name not in self.people:
+            print(f"Friendship not created. {person1_name} doesn't exist!")
+            return
+
+        if person2_name not in self.people:
+            print(f"Friendship not created. {person2_name} doesn't exist!")
+            return
+
+        person1 = self.people[person1_name]
+        person2 = self.people[person2_name]
+
+        # Bidirectional friendship
+        person1.add_friend(person2)
+        person2.add_friend(person1)
+
+    def print_network(self):
+        for person in self.people.values():
+            friend_names = [friend.name for friend in person.friends]
+            print(f"{person.name} is friends with: {', '.join(friend_names)}")
 
 
+# Testing
 
-# Test your code here
+network = SocialNetwork()
+
+# Add people
+network.add_person("Alex")
+network.add_person("Jordan")
+network.add_person("Morgan")
+network.add_person("Taylor")
+network.add_person("Casey")
+network.add_person("Riley")
+
+# Test duplicate
+network.add_person("Alex")
+
+# Create friendships
+network.add_friendship("Alex", "Jordan")
+network.add_friendship("Alex", "Morgan")
+network.add_friendship("Jordan", "Taylor")
+network.add_friendship("Morgan", "Casey")
+network.add_friendship("Taylor", "Riley")
+network.add_friendship("Casey", "Riley")
+network.add_friendship("Morgan", "Riley")
+network.add_friendship("Alex", "Taylor")
+
+# Test missing person
